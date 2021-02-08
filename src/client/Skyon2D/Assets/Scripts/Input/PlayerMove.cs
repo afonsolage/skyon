ffe2530e65d09f6d522eb60a890541b1aa2f084e
Afonso Lage <lage.afonso@gmail.com>
2021-02-08 13:24:36 -0300

    Finished basic movement animations

--- src/client/Skyon2D/Assets/Scripts/Input/PlayerMove.cs
+++ src/client/Skyon2D/Assets/Scripts/Input/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +7,29 @@ public class PlayerMove : MonoBehaviour
 {
 
     public float MoveSpeed = 10f;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var moveHorizontal = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
-        var moveVertical = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        var moveDir = move * MoveSpeed * Time.deltaTime;
+        var mag = moveDir.magnitude;
 
-        transform.Translate(new Vector3(moveHorizontal, moveVertical, 0));
+        _animator.SetFloat("Magnitude", mag);
+
+        if (mag > 0.001)
+        {
+            _animator.SetFloat("Horizontal", move.x);
+            _animator.SetFloat("Vertical", move.y);
+            transform.Translate(moveDir);
+        }
     }
+
 }
